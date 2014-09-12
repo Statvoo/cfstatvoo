a591a65f0434cbbbdfadeab11b3350e6b373e2d9
ao <andrew@hsmoore.com>
2014-09-12 23:26:29 +0100

    update scripts

--- public/javascripts/statvoo.js
+++ public/javascripts/statvoo.js
@@ -1,6 +1,6 @@
 
-<!-- Piwik via CloudFlare Apps -->
+<!-- Statvoo via CloudFlare Apps -->
 
-<!-- http://piwik.org/docs/javascript-tracking/ -->
+(function(){ var sac = document.createElement('script'); sac.src = 'https://static.statvoo.com/js/statvoo-min.js'; sac.type = 'text/javascript'; sac.async = true; var sasc = document.getElementsByTagName('script')[0]; sasc.parentNode.insertBefore(sac, sasc); })();
 
-<!-- End Piwik via CloudFlare Apps Code -->
+<!-- End Statvoo via CloudFlare Apps Code -->
